9a1c7d63879f36ee14d542e052beceea30c6ae4b
Rahul Suresh <rahs98@gmail.com>
2023-08-06 17:02:37 -0400

    A5 Exercises

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./users/users-controller.js";
+
+const app = express();
+app.use(express.json());
+
+HelloController(app);
+UserController(app);
+
+app.listen(4000);
