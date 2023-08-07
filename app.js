1de5495046c75e20b2d20fc579b6f0751d8845f9
Rahul Suresh <rahs98@gmail.com>
2023-08-06 22:37:20 -0400

    Session aware front end

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import session from "express-session";
+import AuthController from "./users/auth-controller.js";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:3000",
+    })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+app.use(session(sessionOptions));
 
+app.use(express.json());
+const port = process.env.PORT || 4000;
+
+AuthController(app);
 TuitsController(app);
 HelloController(app);
 UserController(app);
